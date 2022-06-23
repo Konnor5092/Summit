617571fde70d8ff300ca83b2579182bae739716e
Matt OConnor <matty_o_connor01@hotmail.com>
2022-06-23 08:54:12 +0100

    first commit

---
+++ domain-services/listing/listing-api/ListingApi/Controllers/ListingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ListingApi.Controllers
+{
+    public class ListingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ domain-services/platform/Summit.Platform/Summit.Platform.WebHostCustomization/WebHostExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
+namespace Summit.Platform.WebHostCustomization
+{
+    public static class WebHostExtensions
+    {
+        public static void MigrateDbContext(this IHost webHost)
+        {
+
+        }
+
+        //public static void MigrateDbContext<TContext>(this IHost webHost) where TContext : DbContext
+        //{
+
+        //}
+    }
+}
