aef3bc4cc0e4f47d52e5300c893bd1434f56c082
Matt O'Connor <matty_o_connor01@hotmail.com>
2022-06-28 09:21:12 +0100

    Listing seed updates

---
+++ domain-services/listing/listing-api/ListingApi/Infrastructure/ListingContext.cs
@@ -0,0 +1,25 @@
+﻿using ListingApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListingApi.Infrastructure;
+
+public class ListingContext : DbContext
+{
+    public ListingContext(DbContextOptions<ListingContext> options) : base(options) { }
+
+    public DbSet<Listing> Listings { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder
+            .Entity<Listing>()
+            .ToTable("Listings")
+            .HasKey(l => l.Id);
+
+        builder
+            .Entity<Listing>()
+            .Property(l => l.Type)
+            .HasConversion(l => l.ToString(), l => (ListingType)Enum.Parse(typeof(ListingType), l));
+
+    }
+}
--- domain-services/listing/listing-api/ListingApi/Infrastructure/ListingSeed.cs
+++ domain-services/listing/listing-api/ListingApi/Infrastructure/ListingSeed.cs
@@ -1,6 +1,72 @@
-﻿namespace ListingApi.Infrastructure
+﻿using ListingApi.Models;
+
+namespace ListingApi.Infrastructure;
+
+public class ListingSeed
 {
-    public class ListingSeed
+
+    private IEnumerable<Listing> GetListingsFromFile(string contentRootPath, ILogger<ListingSeed> logger)
     {
+        string csvListings = Path.Combine(contentRootPath, "Infrastructure", "SeedFiles", "Listings.csv");
+
+        string[] csvHeaders;
+
+        try
+        {
+            string[] headers = { "Id", "Name", "Description", "Name", "Price", "Type" };
+            csvHeaders = GetHeaders(csvListings, headers);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "SEED ERROR: {Message}", ex.Message);
+            // Get preconfiguredlistings
+        }
+
+        //return File.ReadAllLines(csvListings).Skip(1).
+    }
+
+    private Listing CreateListing(string[] column, string[] headers)
+    {
+        if (column.Count() != headers.Count())
+        {
+            throw new Exception($"column count '{column.Count()}' not the same as headers count'{headers.Count()}'");
+        }
+
+        return new Listing()
+        {
+            Id = Guid.Parse(column[Array.IndexOf(headers, "id")].Trim('"').Trim()),
+            Name = column[Array.IndexOf(headers, "name")].Trim('"').Trim(),
+            Description = column[Array.IndexOf(headers, "description")].Trim('"').Trim(),
+            Price = int.Parse(column[Array.IndexOf(headers, "Price")].Trim('"').Trim()),
+            Type = (ListingType)Enum.Parse(typeof(ListingType), column[Array.IndexOf(headers, "Price")].Trim('"').Trim())
+        };
+    }
+
+    private string[] GetHeaders(string csvfile, string[] requiredHeaders, string[] optionalHeaders = null)
+    {
+        string[] csvheaders = File.ReadLines(csvfile).First().ToLowerInvariant().Split(',');
+
+        if (csvheaders.Count() < requiredHeaders.Count())
+        {
+            throw new Exception($"requiredHeader count '{requiredHeaders.Count()}' is bigger then csv header count '{csvheaders.Count()}' ");
+        }
+
+        if (optionalHeaders != null)
+        {
+            if (csvheaders.Count() > (requiredHeaders.Count() + optionalHeaders.Count()))
+            {
+                throw new Exception($"csv header count '{csvheaders.Count()}'  is larger then required '{requiredHeaders.Count()}' and optional '{optionalHeaders.Count()}' headers count");
+            }
+        }
+
+        foreach (var requiredHeader in requiredHeaders)
+        {
+            if (!csvheaders.Contains(requiredHeader))
+            {
+                throw new Exception($"does not contain required header '{requiredHeader}'");
+            }
+        }
+
+        return csvheaders;
     }
 }
